08efcf8b9a7fea308ac1d2df1861aa9a436da03c
Andrés <carlosraxon019@gmail.com>
2026-03-18 21:33:12 -0600

    refactor: change list wrappers on navigation

--- components/shared/main-navigation.tsx
+++ components/shared/main-navigation.tsx
@@ -10,10 +10,8 @@ export const MainNavigation = ({ open: _open = false }: { open?: boolean }) => {
   const [open, setOpen] = useState(_open)
 
   const closeMenuAndTransition = () => {
-    // Wrap the state update in startTransition
     startTransition(() => {
       setOpen(false)
-      // Other state updates that trigger navigation or content change can go here
     })
   }
   return (
@@ -39,81 +37,85 @@ export const MainNavigation = ({ open: _open = false }: { open?: boolean }) => {
             ? 'pointer-events-auto visible z-10 opacity-100'
             : 'pointer-events-none invisible z-0 opacity-0'
         )}>
-        <ul className='mx-auto grid w-11/12 laptop:flex laptop:w-full laptop:items-center laptop:justify-between laptop:gap-8'>
+        <div className='mx-auto grid w-11/12 laptop:flex laptop:w-full laptop:items-center laptop:justify-between laptop:gap-8'>
           <div className='contents w-1/2 laptop:flex laptop:items-center laptop:justify-start laptop:gap-x-12 desktop:gap-x-16'>
-            <li
-              className={cn(
-                'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
-                open && 'scale-100 opacity-100 delay-50'
-              )}>
-              <Link
-                onClick={closeMenuAndTransition}
-                prefetch={false}
-                className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
-                href='/ubicaciones'>
-                Ubicaciones <ArrowRight className='size-4 laptop:hidden' />
-              </Link>
-            </li>
-            <li
-              className={cn(
-                'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
-                open && 'scale-100 opacity-100 delay-100'
-              )}>
-              <Link
-                prefetch={false}
-                onClick={closeMenuAndTransition}
-                className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
-                href='/menu'>
-                Menú <ArrowRight className='size-4 laptop:hidden' />
-              </Link>
-            </li>
-            <li
-              className={cn(
-                'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
-                open && 'scale-100 opacity-100 delay-150'
-              )}>
-              <Link
-                prefetch={false}
-                onClick={closeMenuAndTransition}
-                className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
-                href='/nosotros'>
-                Nosotros <ArrowRight className='size-4 laptop:hidden' />
-              </Link>
-            </li>
+            <ul className='contents'>
+              <li
+                className={cn(
+                  'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
+                  open && 'scale-100 opacity-100 delay-50'
+                )}>
+                <Link
+                  onClick={closeMenuAndTransition}
+                  prefetch={false}
+                  className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
+                  href='/ubicaciones'>
+                  Ubicaciones <ArrowRight className='size-4 laptop:hidden' />
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
+                  open && 'scale-100 opacity-100 delay-100'
+                )}>
+                <Link
+                  prefetch={false}
+                  onClick={closeMenuAndTransition}
+                  className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
+                  href='/menu'>
+                  Menú <ArrowRight className='size-4 laptop:hidden' />
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
+                  open && 'scale-100 opacity-100 delay-150'
+                )}>
+                <Link
+                  prefetch={false}
+                  onClick={closeMenuAndTransition}
+                  className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
+                  href='/nosotros'>
+                  Nosotros <ArrowRight className='size-4 laptop:hidden' />
+                </Link>
+              </li>
+            </ul>
           </div>
 
           <div className='contents w-1/2 laptop:flex laptop:items-center laptop:justify-end laptop:gap-x-12 desktop:gap-x-16'>
-            <li
-              className={cn(
-                'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:-mr-2 laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
-                open && 'scale-100 opacity-100 delay-200'
-              )}>
-              <Link
-                onClick={closeMenuAndTransition}
-                prefetch={false}
-                className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
-                href='/contacto'>
-                Contacto <ArrowRight className='size-4 laptop:hidden' />
-              </Link>
-            </li>
-            <li
-              className={cn(
-                'flex w-full scale-95 items-center opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
-                open && 'scale-100 opacity-100 delay-250'
-              )}>
-              <Link
-                onClick={closeMenuAndTransition}
-                prefetch={false}
+            <ul className='contents'>
+              <li
                 className={cn(
-                  buttonVariants(),
-                  'flex h-auto w-full items-center justify-between gap-4 rounded-none px-2 py-6 text-lg laptop:h-9 laptop:w-fit laptop:rounded-base laptop:px-4 laptop:py-0 laptop:text-base laptop:squircle'
-                )}
-                href='/reservaciones'>
-                Reservar Mesa <ArrowRight className='size-4 laptop:hidden' />
-              </Link>
-            </li>
+                  'flex w-full scale-95 items-center border-b opacity-0 duration-150 ease-in-out laptop:-mr-2 laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
+                  open && 'scale-100 opacity-100 delay-200'
+                )}>
+                <Link
+                  onClick={closeMenuAndTransition}
+                  prefetch={false}
+                  className='flex w-full items-center justify-between gap-4 px-2 py-6 text-lg text-white active:bg-white/10 laptop:w-fit laptop:p-0 laptop:text-base laptop:decoration-1 laptop:hover:underline laptop:active:bg-transparent'
+                  href='/contacto'>
+                  Contacto <ArrowRight className='size-4 laptop:hidden' />
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  'flex w-full scale-95 items-center opacity-0 duration-150 ease-in-out laptop:w-fit laptop:scale-100 laptop:border-0 laptop:opacity-100',
+                  open && 'scale-100 opacity-100 delay-250'
+                )}>
+                <Link
+                  onClick={closeMenuAndTransition}
+                  prefetch={false}
+                  className={cn(
+                    buttonVariants(),
+                    'flex h-auto w-full items-center justify-between gap-4 rounded-none px-2 py-6 text-lg laptop:h-9 laptop:w-fit laptop:rounded-base laptop:px-4 laptop:py-0 laptop:text-base laptop:squircle'
+                  )}
+                  href='/reservaciones'>
+                  Reservar Mesa <ArrowRight className='size-4 laptop:hidden' />
+                </Link>
+              </li>
+            </ul>
           </div>
-        </ul>
+        </div>
       </nav>
     </div>
   )

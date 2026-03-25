a7d5b1363b20a672aa668e48310f4dbc22729966
Andrés <carlosraxon019@gmail.com>
2026-03-25 09:58:50 -0600

    feat: add link to focus main content

--- components/shared/site-header.tsx
+++ components/shared/site-header.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import { MainNavigation } from '@/components/shared/main-navigation'
 import { IMAGES } from '@/lib/constants/paths'
@@ -10,7 +11,13 @@ export const SiteHeader = () => {
         'fixed top-0 left-0 z-50 w-full transition-all duration-200 ease-in-out has-data-open:bg-black/80',
         'bg-black'
       )}>
-      <div className='container mx-auto flex h-12 w-11/12 items-center justify-between'>
+      <div className='relative container mx-auto flex h-12 w-11/12 items-center justify-between'>
+        <a
+          href='#contenido-principal'
+          className='sr-only gap-2 bg-white text-center text-lg font-medium text-primary underline decoration-1 focus:not-sr-only focus:absolute focus:inset-0 focus:z-1000 focus:flex focus:size-full focus:items-center focus:justify-center'>
+          Ir al contenido principal
+          <ArrowRight className='size-4' />
+        </a>
         <div className='flex h-full items-center justify-center laptop:absolute laptop:inset-0 laptop:left-1/2 laptop:z-50 laptop:w-fit laptop:-translate-x-1/2'>
           <Link prefetch={false} href={'/'} aria-label='Inicio'>
             <img

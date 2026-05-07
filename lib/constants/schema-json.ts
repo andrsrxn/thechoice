e9557870ec8a060885156cd89cb00814bcb41064
Andrés <carlosraxon019@gmail.com>
2026-05-07 11:25:25 -0600

    fix: remove unused schema comment

--- lib/constants/schema-json.ts
+++ lib/constants/schema-json.ts
@@ -10,23 +10,3 @@ export const WEBSITE_SCHEMA: WithContext<WebSite> = {
   description: COMPANY.DESCRIPTION,
   inLanguage: 'es',
 }
-
-// export const SERVICES_SCHEMA: WithContext<ProfessionalService> = {
-//   '@context': 'https://schema.org',
-//   '@type': 'ProfessionalService',
-//   url: SITE.BASE_URL,
-//   name: COMPANY.NAME,
-//   areaServed: 'Guatemala',
-//   currenciesAccepted: ['GTQ', 'USD'],
-//   image: IMAGES.BRAND.LOGO.PNG.URL,
-
-//   address: {
-//     '@type': 'PostalAddress',
-//     availableLanguage: ['es-419', 'es', 'en-US', 'en-GB'],
-//     postalCode: COMPANY.ADDRESS.POSTAL_CODE,
-//     addressRegion: COMPANY.ADDRESS.REGION,
-//     addressLocality: COMPANY.ADDRESS.LOCALITY,
-//     addressCountry: COMPANY.ADDRESS.COUNTRY,
-//   },
-//   priceRange: '$$$',
-// }

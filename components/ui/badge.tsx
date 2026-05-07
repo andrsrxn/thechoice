eedabe44a316f4e7ec0253dc23f1e7db7f794e55
Andrés <carlosraxon019@gmail.com>
2026-05-07 11:27:43 -0600

    style: format files

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -11,7 +11,7 @@ const badgeVariants = cva(
       variant: {
         default: 'bg-primary text-primary-foreground [a]:hover:bg-primary/80',
         secondary: 'bg-secondary text-secondary-foreground [a]:hover:bg-secondary/80',
-        destructive: 'bg-destructive text-destructive-foreground',
+        destructive: 'text-destructive-foreground bg-destructive',
         success: 'bg-success text-success-foreground',
         outline: 'border-border text-foreground [a]:hover:bg-muted [a]:hover:text-muted-foreground',
         ghost: 'hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50',
